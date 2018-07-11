7de2923f991976fd69ff4ecd33aa407108af13cc
Daniil <daniil.boyko.2017@gmail.com>
2018-07-11 21:50:16 +0300

    Create DalAuction

--- Auction-Service/DAL/Models/DalAuction.cs
+++ Auction-Service/DAL/Models/DalAuction.cs
@@ -14,6 +14,7 @@ namespace DAL.Models
         /// </summary>
         [Key]
         public int Id { get; set; }
+
         /// <summary>
         /// Gets or sets name of the auctioin.
         /// </summary>
